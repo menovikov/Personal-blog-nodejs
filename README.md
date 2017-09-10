26bec6e9a73227277640b98e90a9644a280bb038
Maxim Novikov <mnovikov@ccsteam.ru>
2017-09-11 00:52:27 +0300

    Added readme.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Personal blog on Node JS
+This is a simple personal blog based on Node JS. It copies functionality of https://maxnovikov.com in a different language.
+
+
+## Features not implemented yet
+- Database write queries
+- Subscribers tracking
--- app.js
+++ app.js
@@ -8,28 +8,10 @@ var device = require('express-device');
 app.set('view engine', 'ejs');
 app.set('view options', { layout: true });
 app.use(device.capture());
-
-
 app.use('/static', express.static('./static'));
 
 // fire controllers
 postController(app);
 profileController(app);
 
-
-
-//try
-// function tryArgs(...args){
-// 	console.log(arguments[0]);
-// };
-
-// tryArgs({a:1, b:2, c:3});
-
-
-
-
-
-//endtry
-
 app.listen(3000);
-
